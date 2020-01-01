f90cfd7b4a77571bc08523a254b19a0478e39c14
georgiev032 <50078092+georgiev032@users.noreply.github.com>
2020-01-01 21:39:24 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+var myAnimation = new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.3,
+    image1: 'img/model-1.jpg',
+    image2: 'img/model-2.jpg',
+    displacementImage: 'img/diss.png'
+});
